2c6a986ac3b5bedd8f057cd9c0734dd4b19cc820
Guilherme Mesquita <guilhermer_mesquita@hotmail.com>
2022-05-21 12:02:25 -0400

    jokenpor

---
+++ script.js
@@ -0,0 +1,55 @@
+function pedra(){
+
+    jogador = Math.floor(Math.random()*6)+1
+    computador = Math.floor(Math.random()*6)+1
+
+    if (computador > jogador){
+        alert("Você perdeu! o Computador escolheu Papel :/")
+
+    } else if (computador < jogador){
+        alert("Parabéns! Você Venceu!")
+
+    } else if (computador == jogador){
+        alert("Empate!")
+    }
+}
+
+function papel(){
+
+    jogador = Math.floor(Math.random()*6)+1
+    computador = Math.floor(Math.random()*6)+1
+
+    if (computador > jogador){
+        alert("Você perdeu! O Computador escolheu Tesoura :/")
+
+    } else if (computador < jogador){
+        alert("Parabéns! Você Venceu!")
+
+    } else if (computador == jogador){
+        alert("Empate!")
+    }
+}
+
+function tesoura(){
+
+    jogador = Math.floor(Math.random()*6)+1
+    computador = Math.floor(Math.random()*6)+1
+
+    if (computador > jogador){
+        alert("Você perdeu! O Computador escolheu Pedra :/")
+
+    } else if (computador < jogador){
+        alert("Parabéns! Você Venceu!")
+
+    } else if (computador == jogador){
+        alert("Empate!")
+    }
+}
+
+pedra()
+papel()
+tesoura()
+
+
+
+
